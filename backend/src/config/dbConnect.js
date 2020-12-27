21b6caf8c75203e27dd9faafd5e21eee5ede9657
Anton <anton.sipin@gmail.com>
2020-12-27 05:27:47 +0300

    Added Atlas DB

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const dbConnectionURL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// const dbConnectionURL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const dbConnectionURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3wzbj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 function dbConnect() {
   mongoose.connect(dbConnectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
