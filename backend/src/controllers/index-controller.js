f67c86ae0bc2f88ef5344cb025df061c77d5708d
Anton <anton.sipin@gmail.com>
2020-12-28 16:11:35 +0300

    Added index.html

--- backend/src/controllers/index-controller.js
+++ backend/src/controllers/index-controller.js
@@ -4,7 +4,7 @@ const path = require('path')
 const index = async (req, res) => {
 
   try {
-    res.sendFile(path.resolve('../../../frontend/build/index'))
+    res.sendFile(path.resolve('/frontend/build/index.html'))
   } catch (err) {
     res.sendStatus(500)
   }
